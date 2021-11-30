d7a3a1678e36736db3a1ed90086b4d0982406747
Rafaero <rafaelpaivavitor@hotmail.com>
2021-11-30 14:29:45 -0300

    first commit

---
+++ config/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = express.Router();
+
+let data = []
+
+routes.get('/', (req, res) => {
+    return res.json(data)
+})
+
+routes.post('/add', (req, res) => {
+
+    const body = req.body
+
+    if (!body) {
+
+        return res.status(400).end()
+
+    } else {
+
+        data.push(body)
+        return res.json(body);
+
+    }
+})
+
+module.exports = routes;
