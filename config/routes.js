11106e3d4fc89cc9a4422db5c812e0be5180c61d
Rafael <rafaelpaivavitor@hotmail.com>
2021-12-03 16:23:49 -0300

    change

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-let data = []
+let id = "";
 
 routes.get('/', (req, res) => {
     return res.json(data)
@@ -17,8 +17,32 @@ routes.post('/add', (req, res) => {
 
     } else {
 
-        data.push(body.data.card.id)
-        return res.json(body);
+        id = body.data.card.id
+        const options = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE4MzUxODYsImVtYWlsIjoicmFmYWVsLnZpdG9yQGtleXJ1cy5jb20uYnIiLCJhcHBsaWNhdGlvbiI6MzAwMTI1MjMwfX0.pkyrXEz6FYJA3c9-t7iAg4GHBhAxGcL1SwISob6mH57_WvWY2eMcHiNUt389ouWo8mTVcrwV1Z4fcVB-1AATvg',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `mutation{
+                updateCardField(input: {
+                card_id: "${id}"
+                field_id: "texto_curto"
+                new_value: "Rafael de Paiva"
+                }) {
+                  clientMutationId
+                  success
+                }
+      }`})
+
+
+        };
+        fetch('https://api.pipefy.com/graphql', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
 
     }
 })
